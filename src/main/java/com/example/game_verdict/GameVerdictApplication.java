dd76f38658624dc1b7d918eb069c237406cc136f
SamiChafik <sami.chafik00@gmail.com>
2025-06-14 13:47:27 +0100

    project struct

---
+++ src/main/java/com/example/game_verdict/GameVerdictApplication.java
@@ -0,0 +1,13 @@
+package com.example.game_verdict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameVerdictApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameVerdictApplication.class, args);
+    }
+
+}
