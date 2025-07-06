1d0b4bc13cb8fe8c50b2e700b558782551e7a339
SamiChafik <sami.chafik00@gmail.com>
2025-07-06 15:53:31 +0100

    user controller

---
+++ src/main/java/com/example/game_verdict/controllers/UserController.java
@@ -0,0 +1,58 @@
+package com.example.game_verdict.controllers;
+
+import com.example.game_verdict.services.UserService;
+import com.example.game_verdict.dtos.UserDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+@CrossOrigin("*")
+public class UserController {
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
+        UserDTO createdUser = service.createUser(userDTO);
+        return ResponseEntity.ok(createdUser);
+    }
+
+//    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/userList")
+    public List<UserDTO> getUsers(){
+        return service.getUsersList();
+    }
+
+//    @PreAuthorize("hasAnyRole('ADMIN', 'DRIVER', 'SENDER')")
+    @PutMapping("/updateUser/{id}")
+    public UserDTO updateUser(@RequestBody UserDTO dto, @PathVariable Long id){
+        return service.updateUser(id,dto);
+    }
+
+//    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("/updateRole/{id}")
+    public UserDTO updateUserRole(@RequestBody UserDTO dto, @PathVariable Long id){
+        return service.updateUserRole(id,dto);
+    }
+
+//    @PreAuthorize("hasAnyRole('ADMIN', 'MODERATOR')")
+    @PutMapping("/updateRole/{id}")
+    public UserDTO updateUserStatus(@RequestBody UserDTO dto, @PathVariable Long id){
+        return service.updateUserStatus(id,dto);
+    }
+
+//    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/deleteUser/{id}")
+    public void deleteUser(@PathVariable Long id){
+        service.deleteUser(id);
+    }
+
+}
