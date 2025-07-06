0b7f62c54560c154c063e4d3e09e948d9e74c929
SamiChafik <sami.chafik00@gmail.com>
2025-07-06 15:54:06 +0100

    user repo

---
+++ src/main/java/com/example/game_verdict/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.game_verdict.repositories;
+
+import com.example.game_verdict.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
