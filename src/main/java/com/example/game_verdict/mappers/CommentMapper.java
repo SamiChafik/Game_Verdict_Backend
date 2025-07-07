6174eb1695ce58ab4e9aed36076365ad3aa789fd
SamiChafik <sami.chafik00@gmail.com>
2025-07-07 23:11:47 +0100

    comment mapping added

--- src/main/java/com/example/game_verdict/mappers/CommentMapper.java
+++ src/main/java/com/example/game_verdict/mappers/CommentMapper.java
@@ -4,12 +4,21 @@ package com.example.game_verdict.mappers;
 import com.example.game_verdict.dtos.CommentDTO;
 import com.example.game_verdict.entities.Comment;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CommentMapper {
+
+    @Mapping(target = "review", ignore = true)
+    @Mapping(target = "member", ignore = true)
     Comment toEntity(CommentDTO dto);
+
+    @Mapping(target = "reviewId", source = "review.id")
+    @Mapping(target = "memberId", source = "member.id")
+    @Mapping(target = "memberUsername", source = "member.username")
     CommentDTO toDTO(Comment comment);
+
     List<CommentDTO> toDTOs(List<Comment> comments);
 }
