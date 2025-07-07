16c3eef4a4ad6264bc757a3a644e637c65020b41
SamiChafik <sami.chafik00@gmail.com>
2025-07-07 23:12:21 +0100

    find by reviewId func added

--- src/main/java/com/example/game_verdict/repositories/CommentRepository.java
+++ src/main/java/com/example/game_verdict/repositories/CommentRepository.java
@@ -2,6 +2,8 @@ package com.example.game_verdict.repositories;
 
 import com.example.game_verdict.entities.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByReviewId(Long reviewId);
 }
