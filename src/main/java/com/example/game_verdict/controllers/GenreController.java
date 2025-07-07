22e900d271252863c44528e065611d8689d9aa23
SamiChafik <sami.chafik00@gmail.com>
2025-07-07 01:09:30 +0100

    genre controller

--- src/main/java/com/example/game_verdict/controllers/GenreController.java
+++ src/main/java/com/example/game_verdict/controllers/GenreController.java
@@ -1,4 +1,52 @@
 package com.example.game_verdict.controllers;
 
+import com.example.game_verdict.dtos.GenreDTO;
+import com.example.game_verdict.services.GenreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/genres")
 public class GenreController {
+
+    private final GenreService genreService;
+
+    @Autowired
+    public GenreController(GenreService genreService) {
+        this.genreService = genreService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<GenreDTO>> getAllGenres() {
+        List<GenreDTO> genres = genreService.getAllGenres();
+        return ResponseEntity.ok(genres);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GenreDTO> getGenreById(@PathVariable Long id) {
+        GenreDTO genre = genreService.getGenreById(id);
+        return ResponseEntity.ok(genre);
+    }
+
+    @PostMapping
+    public ResponseEntity<GenreDTO> createGenre(@RequestBody GenreDTO genreDTO) {
+        GenreDTO createdGenre = genreService.createGenre(genreDTO);
+        return new ResponseEntity<>(createdGenre, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<GenreDTO> updateGenre(@PathVariable Long id, @RequestBody GenreDTO genreDTO) {
+        GenreDTO updatedGenre = genreService.updateGenre(id, genreDTO);
+        return ResponseEntity.ok(updatedGenre);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteGenre(@PathVariable Long id) {
+        genreService.deleteGenre(id);
+        return ResponseEntity.noContent().build();
+    }
 }
