9fe2cb20d470f67c5e9328154ce19864264d98d0
SamiChafik <sami.chafik00@gmail.com>
2025-07-07 23:01:55 +0100

    review repo

--- src/main/java/com/example/game_verdict/repositories/ReviewRepository.java
+++ src/main/java/com/example/game_verdict/repositories/ReviewRepository.java
@@ -2,6 +2,8 @@ package com.example.game_verdict.repositories;
 
 import com.example.game_verdict.entities.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findByGameId(Long gameId);
 }
