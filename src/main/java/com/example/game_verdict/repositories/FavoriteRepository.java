182f6d6de9ecfb7e25b47a373148436d64e55266
SamiChafik <sami.chafik00@gmail.com>
2025-07-09 10:01:27 +0100

    deleted

--- src/main/java/com/example/game_verdict/repositories/FavoriteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.game_verdict.repositories;
-
-import com.example.game_verdict.entities.FavoriteGame;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FavoriteRepository extends JpaRepository<FavoriteGame, Long> {
-}
