d03154a50e492ffc589bf73225e7673a7dc73593
SamiChafik <sami.chafik00@gmail.com>
2025-09-04 19:39:39 +0100

    enabled PreAuthorize for each request

--- src/main/java/com/example/game_verdict/controllers/UserController.java
+++ src/main/java/com/example/game_verdict/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.example.game_verdict.dtos.UserDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 //import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,31 +26,31 @@ public class UserController {
 //        return ResponseEntity.ok(createdUser);
 //    }
 
-//    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/userList")
     public List<UserDTO> getUsers(){
         return service.getUsersList();
     }
 
-//    @PreAuthorize("hasAnyRole('ADMIN', 'DRIVER', 'SENDER')")
+    @PreAuthorize("hasAnyRole('ADMIN', 'DRIVER', 'SENDER')")
     @PutMapping("/updateUser/{id}")
     public UserDTO updateUser(@RequestBody UserDTO dto, @PathVariable Long id){
         return service.updateUser(id,dto);
     }
 
-//    @PreAuthorize("hasRole('ADMIN')")
-    @PutMapping("/role/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("/updateRole/{id}")
     public UserDTO updateUserRole(@RequestBody UserDTO dto, @PathVariable Long id){
         return service.updateUserRole(id,dto);
     }
 
-//    @PreAuthorize("hasAnyRole('ADMIN', 'MODERATOR')")
-    @PutMapping("/updateRole/{id}")
-    public UserDTO updateUserStatus(@RequestBody UserDTO dto, @PathVariable Long id){
-        return service.updateUserStatus(id,dto);
+    @PreAuthorize("hasAnyRole('ADMIN', 'MODERATOR')")
+    @PutMapping("/updateStatus/{id}")
+    public UserDTO updateUserStatus(@RequestBody boolean isBanned, @PathVariable Long id){
+        return service.updateUserStatus(id, isBanned);
     }
 
-//    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/deleteUser/{id}")
     public void deleteUser(@PathVariable Long id){
         service.deleteUser(id);
