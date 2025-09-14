e271571277b83966a7047216d71443948dc4fce3
SamiChafik <sami.chafik00@gmail.com>
2025-09-14 21:30:40 +0100

    new func to delete current user

--- src/main/java/com/example/game_verdict/services/UserService.java
+++ src/main/java/com/example/game_verdict/services/UserService.java
@@ -1,15 +1,13 @@
 package com.example.game_verdict.services;
 
 import com.example.game_verdict.dtos.UserDTO;
-import com.example.game_verdict.entities.Role;
 import com.example.game_verdict.entities.User;
 import com.example.game_verdict.mappers.UserMapper;
 import com.example.game_verdict.repositories.UserRepository;
-//import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
+import java.security.Principal;
 import java.util.List;
 
 @Service
@@ -24,66 +22,28 @@ public class UserService {
         this.passwordEncoder = passwordEncoder;
     }
 
-//    public UserDTO createUser(UserDTO userDTO) {
-//        if (userDTO.getEmail() == null || userDTO.getEmail().isEmpty()) {
-//            throw new IllegalArgumentException("Email is required");
-//        }
-//        if (userDTO.getPassword() == null || userDTO.getPassword().isEmpty()) {
-//            throw new IllegalArgumentException("Password is required");
-//        }
-//
-//        User existingUser = repository.findByEmail(userDTO.getEmail());
-//        if (existingUser != null) {
-//            throw new RuntimeException("Email already in use");
-//        }
-//
-//        User user = new User();
-//        user.setName(userDTO.getName());
-//        user.setEmail(userDTO.getEmail());
-//        user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
-//        user.setPassword(userDTO.getPassword());
-//        user.setAvatar(userDTO.getAvatar());
-//        user.setBio(userDTO.getBio());
-//        user.setCreatedAt(LocalDate.now());
-//        user.setLastLogin(LocalDate.now());
-//        user.setRole(userDTO.getRole() != null ? userDTO.getRole() : Role.MEMBER);
-//        user.setBanned(userDTO.isBanned());
-//
-//        User savedUser = repository.save(user);
-//
-//        return mapper.toDTO(savedUser);
-//    }
-
     public UserDTO updateUser(Long id, UserDTO dto) {
-        User user = repository.findById(id).orElse(null);
-        if (user == null) {
-            throw new RuntimeException("user not found");
-        }
+        User user = repository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
         user.setName(dto.getName());
         user.setEmail(dto.getEmail());
         user.setBio(dto.getBio());
         user.setAvatar(dto.getAvatar());
-        user.setPassword(passwordEncoder.encode(dto.getPassword()));
-//        user.setPassword(dto.getPassword());
+        if (dto.getPassword() != null && !dto.getPassword().isEmpty()) {
+            user.setPassword(passwordEncoder.encode(dto.getPassword()));
+        }
         User savedUser = repository.save(user);
         return mapper.toDTO(savedUser);
     }
 
     public UserDTO updateUserRole(Long id, UserDTO dto){
-        User user = repository.findById(id).orElse(null);
-        if (user == null){
-            throw new RuntimeException("user not found");
-        }
+        User user = repository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
         user.setRole(dto.getRole());
         User savedUser = repository.save(user);
         return mapper.toDTO(savedUser);
     }
 
     public UserDTO updateUserStatus(Long id, Boolean isBanned){
-        User user = repository.findById(id).orElse(null);
-        if (user == null){
-            throw new RuntimeException("user not found");
-        }
+        User user = repository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
         user.setBanned(isBanned);
         User savedUser = repository.save(user);
         return mapper.toDTO(savedUser);
@@ -96,5 +56,10 @@ public class UserService {
 
     public void deleteUser(Long id){
         repository.deleteById(id);
+    }
+
+    public void deleteCurrentUser(Principal principal) {
+        User user = repository.findByEmail(principal.getName()).orElseThrow(() -> new RuntimeException("User not found"));
+        repository.delete(user);
     }
 }
