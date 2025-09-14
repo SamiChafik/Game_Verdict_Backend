e920f8e41caf1aaa3e93df9465e12a3c069ab76d
SamiChafik <sami.chafik00@gmail.com>
2025-09-14 21:22:47 +0100

    deleted

--- src/main/java/com/example/game_verdict/repositories/PlatformRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.game_verdict.repositories;
-
-import com.example.game_verdict.entities.Platform;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlatformRepository extends JpaRepository<Platform, Long> {
-}
